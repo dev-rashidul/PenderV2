5dcd8941578db91fb5d23e85e5c6994bedc8c84a
dev-rashidul <rashidul.contact@gmail.com>
2023-06-09 23:01:16 +0600

    Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,49 @@
-// JavaScript For Sticky Header
+// JavaScript For Sticky Header 
+
+const bar = document.getElementById("bar");
+const nav = document.querySelector(".nav")
+
+bar.addEventListener("click", ()=>{
+    nav.classList.toggle("active")
+})
+
+// JavaScript For Sticky Header Scroll
 
 window.addEventListener("scroll", function(){
     const header = document.querySelector(".navbar");
     header.classList.toggle("sticky", window.scrollY > 100)
 
+})
+
+// Pet Selling Slider JavaScript
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:25,
+    responsiveClass:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        700:{
+            items:2,
+            nav:true,
+        },
+        1000:{
+            items:3,
+            nav:true,
+        },
+        1200:{
+            items:4,
+            nav:true,
+        }
+    },
+    navText: ['<i class="fa-solid fa-arrow-left"></i>', '<i class="fa-solid fa-arrow-right"></i>']
 })
